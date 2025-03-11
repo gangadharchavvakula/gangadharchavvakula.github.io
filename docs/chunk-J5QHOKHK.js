beaf17c9a2034ea6402c77f7a87409a671e5368f
gangadharchavvakula <gangadharchavvakulaofficial@gmail.com>
2025-03-11 21:30:20 +0530

    Built project to serve from root (/)

--- docs/chunk-J5QHOKHK.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-ABOMOY4E.js";import"./chunk-WQARGITY.js";import"./chunk-HIZOTBJY.js";export{a as ContactComponent};
---
+++ docs/chunk-ZWB6OF3F.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-K52BW6L3.js";import"./chunk-WQARGITY.js";import"./chunk-HIZOTBJY.js";export{a as ContactComponent};
